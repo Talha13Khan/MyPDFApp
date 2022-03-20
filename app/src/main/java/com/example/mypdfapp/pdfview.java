db7e0dd991753c75988c1bd1bd64c7848aeffd5a
khatalha <khatalha2015@gmail.com>
2022-03-21 01:20:29 +0530

    First commit

---
+++ app/src/main/java/com/example/mypdfapp/pdfview.java
@@ -0,0 +1,16 @@
+package com.example.mypdfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pdfview extends AppCompatActivity {
+
+    PDFView app;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdfview);
+    }
+}
